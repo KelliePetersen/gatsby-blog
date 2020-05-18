5d91b9df1324da39cddbf9a716988450474a6ca9
KelliePetersen <kelliepetersen@outlook.com>
2020-05-18 11:00:00 -0600

    create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; Copyright 2020. All rights reserved. Try to resist the urge to steal this stellar design.</p>
+    </footer>
+  )
+}
+
+export default Footer
