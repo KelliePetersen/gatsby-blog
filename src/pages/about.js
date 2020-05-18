f592e0318423208927dca20dce3cb98c3876ab18
KelliePetersen <kelliepetersen@outlook.com>
2020-05-18 10:00:00 -0600

    create about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>Who I am</h1>
+      <p>Someone who spends 10 minutes trying to think of something interesting to write, and ends up with this.</p>
+    </div>
+  )
+}
+
+export default About
