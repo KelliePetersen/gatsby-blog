1aad49aaf282708f297b20318e78a904a2abaed0
KelliePetersen <kelliepetersen@outlook.com>
2020-05-19 12:00:00 -0600

    add custom styling to Header and Layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,13 @@ import styles from './header.module.scss'
 const Header = () => {
   return (
     <div className={styles.header}>
-      <Link className={styles.link} to="/">Kellie Petersen</Link>
+      <Link className={styles.heading} to="/">Kellie Petersen</Link>
       <nav>
         <ul className={styles.list}>
-          <li><Link className={styles.link} to="/">Home</Link></li>
-          <li><Link className={styles.link} to="/about">About</Link></li>
-          <li><Link className={styles.link} to="/blog">Blog</Link></li>
-          <li><Link className={styles.link} to="/contact">Contact</Link></li>
+          <li><Link className={styles.link} activeClassName={styles.activeLink} to="/">Home</Link></li>
+          <li><Link className={styles.link} activeClassName={styles.activeLink} to="/about">About</Link></li>
+          <li><Link className={styles.link} activeClassName={styles.activeLink} to="/blog">Blog</Link></li>
+          <li><Link className={styles.link} activeClassName={styles.activeLink} to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </div>
