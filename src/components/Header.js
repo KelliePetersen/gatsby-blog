80fe1e1036607d09d06b7978dc5a21569ba0008c
KelliePetersen <kelliepetersen@outlook.com>
2020-05-19 12:00:00 -0600

    add styling module for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styles from './header.module.scss'
 
 const Header = () => {
   return (
-    <div>
-      <Link to="/">Kellie Petersen</Link>
+    <div className={styles.header}>
+      <Link className={styles.link} to="/">Kellie Petersen</Link>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/blog">Blog</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+        <ul className={styles.list}>
+          <li><Link className={styles.link} to="/">Home</Link></li>
+          <li><Link className={styles.link} to="/about">About</Link></li>
+          <li><Link className={styles.link} to="/blog">Blog</Link></li>
+          <li><Link className={styles.link} to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </div>
